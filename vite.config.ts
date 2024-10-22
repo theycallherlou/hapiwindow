b5f4d17eb615c4402188c25e78f4d2f8c1ac1173
Adam Robson <adamrayrobson@gmail.com>
2024-10-21 23:20:00 -0700

    add vite config file

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api\/v1/, '')
+      }
+    } 
+  }
+})
